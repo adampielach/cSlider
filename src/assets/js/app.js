93bd6fe09c59a98cb4d0c2b7b78573059a3eeb43
Adam Pielach <adampielach@gmail.com>
2019-11-09 22:42:42 +0100

    minor updates to the script

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,20 +1,46 @@
-class cSlider {
+class CSlider {
   constructor(settings) {
-    this.settings = cSlider.mergeSettings(settings);
+    this.settings = CSlider.mergeSettings(settings);
+
+    this.selector =
+      typeof this.settings.selector === "string"
+        ? document.querySelector(this.settings.selector)
+        : this.settings.selector;
+    if (this.selector === null) {
+      throw new Error("Selector '" + this.selector + "' doesn't exist!");
+    }
+
+    // general settings
+    this.selectorWidth = this.selector.clientWidth;
+    this.selectorChildren = Array.prototype.slice.call(this.selector.children);
+    this.currentIndex = this.settings.startIndex % this.selectorChildren.length;
+
+    this.init();
   }
 
   // default settings
 
-  mergeSettings = function(userSettings) {
+  static mergeSettings(userSettings) {
     let settings = {
-      selector: '.cSlider',
+      selector: ".cSlider",
       duration: 200,
-      easing: 'ease-out',
+      easing: "ease-out",
       perPage: 1,
       startIndex: 0,
       loop: false,
       onInit: () => {},
       onChange: () => {}
+    };
+
+    for (let attrname in userSettings) {
+      settings[attrname] = userSettings[attrname];
     }
+
+    return settings;
   }
+
+  init() {}
 }
+
+let s = new CSlider({ selector: ".c-slide" });
+console.log(s);
